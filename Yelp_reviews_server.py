1b3fc133d31847d72ca910840f723ea6bf9315f4
Shri Charan <36787916+bigb0ss@users.noreply.github.com>
2020-08-06 12:37:00 +0530

    Update Yelp_reviews_server.py

--- Yelp_reviews_server.py
+++ Yelp_reviews_server.py
@@ -71,7 +71,7 @@ def yelpReviewsJob():
 	print("Completed Job")
 
 
-schedule.every().wednesday.at("06:00").do(yelpReviewsJob)
+schedule.every().wednesday.at("07:10").do(yelpReviewsJob)
 
 while True:
 	schedule.run_pending()
